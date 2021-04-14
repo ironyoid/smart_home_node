8d415d7ea24e565d5fcc01fed3adf845e78e1044
Nick <63263860+ironyoid@users.noreply.github.com>
2021-04-14 10:26:36 +0300

    0.0.1 test

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const jsonParser = express.json();
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(url, { useUnifiedTopology: true });
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/pics"));
+// mongoClient.connect(function(err, client){
+//
+//   const db = client.db("usersdb");
+//   const collection = db.collection("users");
+//   let user = {name: "Tom", age: 24};
+//   collection.insertOne(user, function(err, result){
+//
+//     if(err){
+//       return console.log(err);
+//     }
+//     console.log(result.ops);
+//     client.close();
+//   });
+// });
+let data_storage = {
+lamp: "ON",
+  wireless_lamp: "OFF",
+  ir_lamp: "OFF",
+  pump: "OFF",
+  music: "ON",
+  container: "Full",
+  backlight: "ON"
+};
+
+app.post("/buttons", jsonParser, function (request, response) {
+  if(!request.body) return response.sendStatus(400);
+  data_storage[request.body.button] = request.body.state ? "ON" : "OFF";
+  console.log(data_storage);
+  response.json(request.body);
+
+});
+// app.get(/sw_\d\/on$/, function (req, res) {
+//
+//   console.log("switch #" + req.path[4] + " is on");
+//   res.send("hui1");
+// });
+//
+// app.get(/sw_\d\/off$/, function (req, res) {
+//
+//   console.log("switch #" + req.path[4] + " is off");
+//   res.send("hui2");
+// });
+
+app.get("/switch_status", function (req, res) {
+  res.send({"pump_state":data_storage.container, "lamp_state":data_storage.lamp, "wireless_state":data_storage.wireless_lamp,
+    "ir_state":data_storage.ir_lamp, "music_state": data_storage.music, "backlight": data_storage.backlight});
+});
+
+app.listen(3000);
---
+++ public/script.js
@@ -0,0 +1,85 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+   // PageInit();
+
+});
+let colorPicker = new iro.ColorPicker('#picker', {
+    width: 200,
+    height: 200,
+    display: "inline-block",
+    borderWidth: 1,
+    borderColor: "#000",
+});
+function Switch(bt)
+{
+    let chck = document.getElementById(bt.id);
+    let json_to = {
+        "button": chck.id,
+        "state":  chck.checked
+    }
+    console.log(json_to);
+    $.ajax({
+        type: 'post',
+        url: '/buttons',
+        data: JSON.stringify(json_to),
+        contentType: "application/json; charset=utf-8",
+        traditional: true,
+        success: function (data) {
+        console.log(json_to);
+        }
+    });
+
+    // if(chck.checked)
+    // {
+    //     $.get("/" + bt.id + "/on", onAjaxSuccess);
+    // }
+    // else
+    // {
+    //     $.get("/" + bt.id + "/off", onAjaxSuccess);
+    // }
+
+    function onAjaxSuccess(data)
+    {
+
+    }
+}
+
+function MrHide(bt)
+{
+    let chck = document.getElementById(bt.id);
+    console.log(chck.checked);
+
+}
+
+
+let timerId = setTimeout(function request() {
+    $.get("/switch_status", onAjaxSuccess);
+
+    function OnOffToBool(str) {
+        if(str === "ON") return true;
+        else if(str === "OFF") return false  ;
+
+
+    }
+
+
+
+    function onAjaxSuccess(data)
+    {
+        /* sw_1 - wireless, sw_2 - wire, sw_3 - ir, sw_4 - music */
+        document.getElementById("pump_state").innerHTML = data.pump_state.toUpperCase();
+        document.getElementById("lamp_state").innerHTML = data.lamp_state.toUpperCase();
+        document.getElementById("ir_state").innerHTML = data.ir_state.toUpperCase();
+        document.getElementById("wireless_state").innerHTML = data.wireless_state.toUpperCase();
+
+        document.getElementById("lamp").checked = OnOffToBool(data.lamp_state);
+        document.getElementById("wireless_lamp").checked = OnOffToBool(data.wireless_state);
+        document.getElementById("ir_lamp").checked = OnOffToBool(data.ir_state);
+        document.getElementById("music").checked = OnOffToBool(data.music_state);
+
+        document.getElementById('picker_wrap').hidden = !OnOffToBool(data.backlight);
+        document.getElementById('backlight').checked = OnOffToBool(data.backlight);
+        console.log(data);
+    }
+    timerId = setTimeout(request, 1000);
+}, 1000);
